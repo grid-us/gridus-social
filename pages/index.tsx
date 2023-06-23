af8495324f6471dd293339db5333bb6c9c656f05
seodam-hst <seodam.hst@gmail.com>
2023-06-23 15:09:08 +0900

    change UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { SearchDialog } from '@/components/SearchDialog'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Stack } from '@mui/material'
+import { Card } from '@tremor/react'
 
-const inter = Inter({ subsets: ['latin'] })
+import { Dashboard } from '@/components/Dashboard'
+import { Chatbar } from '@/components/Chatbar'
+import { MiniDrawer } from '@/components/MiniDrawer'
 
 export default function Home() {
   return (
@@ -19,39 +18,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          <SearchDialog />
-        </div>
-
-        <div className="py-8 w-full flex items-center justify-center space-x-6">
-          <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-            <Link href="https://supabase.com" className="flex items-center justify-center">
-              <p className="text-base mr-2">Built by Supabase</p>
-              <Image src={'/supabase.svg'} width="20" height="20" alt="Supabase logo" />
-            </Link>
-          </div>
-          <div className="border-l border-gray-300 w-1 h-4" />
-          <div className="flex items-center justify-center space-x-4">
-            <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-              <Link
-                href="https://github.com/supabase/supabase"
-                className="flex items-center justify-center"
-              >
-                <Image src={'/github.svg'} width="20" height="20" alt="Github logo" />
-              </Link>
-            </div>
-            <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-              <Link
-                href="https://twitter.com/supabase"
-                className="flex items-center justify-center"
-              >
-                <Image src={'/twitter.svg'} width="20" height="20" alt="Twitter logo" />
-              </Link>
-            </div>
-          </div>
-        </div>
-      </main>
+      <Stack direction="row" spacing={2}>
+        <MiniDrawer />
+        <Stack spacing={2}>
+          <Card />
+          <Dashboard />
+        </Stack>
+        <Chatbar />
+      </Stack>
     </>
   )
 }
